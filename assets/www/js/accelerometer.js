6c026d8b8e40586f7dc110553e3036d27b2db170
makihiro <makihiro_dn8@nifty.com>
2013-07-14 13:11:13 +0900

    accelerometer : 一定間隔で加速度取得する処理を修正

--- assets/www/js/accelerometer.js
+++ assets/www/js/accelerometer.js
@@ -5,16 +5,14 @@
     // device APIs are available
     //
     function onDeviceReady() {
-        navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
+        // Update acceleration every 3 seconds
+        var options = { frequency: 1000 };
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);        
     }
 
     // onSuccess: Get a snapshot of the current acceleration
     //
     function onSuccess(acceleration) {
-//        alert('Acceleration X: ' + acceleration.x + '\n' +
-//              'Acceleration Y: ' + acceleration.y + '\n' +
-//              'Acceleration Z: ' + acceleration.z + '\n' +
-//              'Timestamp: '      + acceleration.timestamp + '\n');
         $('#val_x').text('Acc X:' + acceleration.x);
         $('#val_y').text('Acc Y:' + acceleration.y);
         $('#val_z').text('Acc Z:' + acceleration.z);
@@ -24,7 +22,7 @@
     // onError: Failed to get the acceleration
     //
     function onError() {
-//        alert('onError!' + e);
+//        alert('onError!');
     }
 	
 	
@@ -32,7 +30,7 @@
     //
 	function onLoad(){
 //		alert("onLoad");
-        setInterval("getAccel()", 1000);
+//        setInterval("getAccel()", 1000);
 	}
 
 	function getAccel(){
