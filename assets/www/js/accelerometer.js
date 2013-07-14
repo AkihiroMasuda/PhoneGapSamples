e08eb8ce7473394e0447feed9d655a8474df279d
makihiro <makihiro_dn8@nifty.com>
2013-07-14 12:58:42 +0900

    Accelerometer: 一定時間毎に更新

--- assets/www/js/accelerometer.js
+++ assets/www/js/accelerometer.js
@@ -11,16 +11,20 @@
     // onSuccess: Get a snapshot of the current acceleration
     //
     function onSuccess(acceleration) {
-        alert('Acceleration X: ' + acceleration.x + '\n' +
-              'Acceleration Y: ' + acceleration.y + '\n' +
-              'Acceleration Z: ' + acceleration.z + '\n' +
-              'Timestamp: '      + acceleration.timestamp + '\n');
+//        alert('Acceleration X: ' + acceleration.x + '\n' +
+//              'Acceleration Y: ' + acceleration.y + '\n' +
+//              'Acceleration Z: ' + acceleration.z + '\n' +
+//              'Timestamp: '      + acceleration.timestamp + '\n');
+        $('#val_x').text('Acc X:' + acceleration.x);
+        $('#val_y').text('Acc Y:' + acceleration.y);
+        $('#val_z').text('Acc Z:' + acceleration.z);
+        $('#val_ts').text('Timestamp:' + acceleration.timestamp);
     }
 
     // onError: Failed to get the acceleration
     //
     function onError() {
-        alert('onError!');
+//        alert('onError!' + e);
     }
 	
 	
@@ -28,6 +32,7 @@
     //
 	function onLoad(){
 //		alert("onLoad");
+        setInterval("getAccel()", 1000);
 	}
 
 	function getAccel(){
