791dbe7fdf383d159e058c4b22b687c5fd1b355f
makihiro <makihiro_dn8@nifty.com>
2013-07-15 19:14:32 +0900

    fileAPIつかってルートフォルダ以下のものを表示してみた

--- src/com/example/adtest56_phonegap/MainActivity.java
+++ src/com/example/adtest56_phonegap/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends DroidGap {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.loadUrl("file:///android_asset/www/top.html");
+//		super.loadUrl("file:///android_asset/www/file.html");
 //		super.loadUrl("file:///android_asset/www/accelerometer.html");
 //		super.loadUrl("file:///android_asset/www/compass.html");
 	}
